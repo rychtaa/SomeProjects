deda8a6222af7ed3cdd43c0bd8586f3ed9a85382
rychta <rychta@gmail.com>
2024-01-30 14:30:50 +0100

    BMI calculator

---
+++ bmi_calc.py
@@ -0,0 +1,22 @@
+# BMI calculator
+print("*"*5 + " BMI calculator " + "*"*5)
+# Height set to right data type
+height = float(input("What is your height?\n"))
+# Weight set to right data type
+weight = int(input("What is your weight?\n"))
+# Calculations
+bmi = weight / height ** 2
+print(f"Your BMI: {bmi:.2f}")
+if bmi < 18.5:
+    print("You are under weight.")
+elif bmi >= 18.5:
+    print("You have normal weight.")
+elif bmi >= 25:
+    print("You are slightly overweight.")
+elif bmi >= 30:
+    print("You are obese.")
+else:
+    print("You are clinically obese")
+
+
+
