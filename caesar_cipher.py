5024f92cd583a8e17da2490149412e3e2f167a2a
rychta <rychta@gmail.com>
2024-02-16 14:22:55 +0100

    caesar cipher

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -34,6 +34,9 @@ def caesar(start_text, shift_amount, cipher_direction):
 should_continue = True
 while should_continue:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    if direction not in ("encode", "decode"):
+        print("Wrong answer, try again.")
+        continue
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
     shift = shift % 26
@@ -41,7 +44,8 @@ while should_continue:
 
     restart = input("Type 'yes' if you want to go again. Otherwise type 'no'\n")
     if restart == "no":
-         should_continue = False
-         print("Goodbye!")
+        should_continue = False
+        print("Goodbye!")
     else:
         print("Wrong answer, try again.")
+        
